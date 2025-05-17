af8f4acaf30ed44a193ee70c3d5ba76a3deacfa3
Marek Stastka <marek.stastka.023@pslib.cz>
2025-05-17 12:21:56 +0200

    commit

---
+++ src/script/swiper.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".swiper-container", {
+        loop: true,
+        autoplay: {
+          delay: 2500,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
