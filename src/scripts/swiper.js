759e00f705ad0ea281303e4a4f9aa4a5bc9a2835
Marek Stastka <marek.stastka.023@pslib.cz>
2025-05-19 12:52:07 +0200

    Add pagination functionality to Swiper slider in swiper.html and swiper.js

--- src/scripts/swiper.js
+++ src/scripts/swiper.js
@@ -1,22 +1,22 @@
-// core version + navigation, pagination modules:
+
 import Swiper from 'swiper';
 import { Navigation, Pagination } from 'swiper/modules';
-// import Swiper and modules styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
-// init Swiper:
-const swiper = new Swiper('.swiper', {
-  // configure Swiper to use modules
-  modules: [Navigation, Pagination],
-  navigation: {
-    nextEl: '.swiper-button-next', // Selector for the "next" button
-    prevEl: '.swiper-button-prev', // Selector for the "previous" button
-  },
-  pagination: {
-    el: '.swiper-pagination', // Selector for the pagination element
-    clickable: true, // Allow clicking on pagination bullets
-  },
-  loop: true, // Enable looping of slides
-});
+var swiper = new Swiper(".swiper-container", { 
+        modules: [Navigation, Pagination],
+        loop: true,
+        autoplay: {
+          delay: 2500,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
