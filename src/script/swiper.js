c749a3734a4e80aa5af31ce06eb5d0aac2fe7294
Marek Stastka <marek.stastka.023@pslib.cz>
2025-05-19 12:16:05 +0200

    commit

--- src/script/swiper.js
+++ src/script/swiper.js
@@ -1,10 +1,22 @@
-var swiper = new Swiper(".swiper-container", {
-        loop: true,
-        autoplay: {
-          delay: 2500,
-        },
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-      });
+// core version + navigation, pagination modules:
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+ 
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+  // configure Swiper to use modules
+  modules: [Navigation, Pagination],
+  navigation: {
+    nextEl: '.swiper-button-next', // Selector for the "next" button
+    prevEl: '.swiper-button-prev', // Selector for the "previous" button
+  },
+  pagination: {
+    el: '.swiper-pagination', // Selector for the pagination element
+    clickable: true, // Allow clicking on pagination bullets
+  },
+  loop: true, // Enable looping of slides
+});
